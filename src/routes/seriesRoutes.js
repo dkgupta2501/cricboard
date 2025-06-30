0238d2efe298e565ca9c5d991147f651a23730db
dkgupta2501 <gupta.dhananjay@gmail.com>
2025-06-30 13:03:22 +0530

    added API

--- src/routes/seriesRoutes.js
+++ src/routes/seriesRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { createSeries, getAllSeries ,assignTeamsToSeries,getTeamsForSeries} = require('../controllers/seriesController');
+const { createSeries, getAllSeries ,assignTeamsToSeries,getTeamsForSeries,getDataValue} = require('../controllers/seriesController');
 
 
 
 router.post('/createSeries', createSeries);
 
 router.get('/getAllSeries', getAllSeries);
+router.get('/getDataValue', getDataValue);
 
 router.post('/:series_id/teams', assignTeamsToSeries);
 router.get('/:series_id/teams', getTeamsForSeries)
