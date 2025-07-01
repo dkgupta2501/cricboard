31faf50838de52cdf262b9cdcf8bfbb0ccf859a6
dkgupta2501 <gupta.dhananjay@gmail.com>
2025-07-01 18:43:55 +0530

    state and city API added

--- src/controllers/regionController.js
+++ src/controllers/regionController.js
@@ -26,3 +26,52 @@ exports.getStatesByCountry = async (req, res) => {
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+exports.getCountryList = async (req, res) => {
+    try {
+      const response = await axios.get('https://countriesnow.space/api/v0.1/countries');
+  
+      if (response.data.error) {
+        return res.status(500).json({ message: 'Error from external API', error: response.data.msg });
+      }
+  
+      return res.status(200).json({
+        countries: response.data.data
+      });
+  
+    } catch (error) {
+      console.error('Error fetching countries:', error.message);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+
+
+  exports.getCitiesByState = async (req, res) => {
+    try {
+      const { country, state } = req.body;
+  
+      if (!country || !state) {
+        return res.status(400).json({ message: 'Country and State are required' });
+      }
+  
+      const response = await axios.post('https://countriesnow.space/api/v0.1/countries/state/cities', {
+        country,
+        state
+      });
+  
+      if (response.data.error) {
+        return res.status(500).json({ message: 'Error from external API', error: response.data.msg });
+      }
+  
+      return res.status(200).json({
+        country,
+        state,
+        cities: response.data.data
+      });
+  
+    } catch (error) {
+      console.error('Error fetching cities:', error.message);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  };
+  
--- src/routes/regionRoutes.js
+++ src/routes/regionRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getStatesByCountry } = require('../controllers/regionController');
+const { getStatesByCountry,getCountryList ,getCitiesByState} = require('../controllers/regionController');
 
 
 
 router.post('/getStatesByCountry', getStatesByCountry);
+router.get('/getCountryList', getCountryList);
+router.post('/getCitiesByState', getCitiesByState);
 
 module.exports = router;
